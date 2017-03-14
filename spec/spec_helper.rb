81c8b111154d32ab070fb35dbcedd374528cf374
ndmanvar <ndmanvar@yahoo.com>
2017-03-13 22:18:57 -0700

    using reset session and parallel_split_test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,20 +4,6 @@ require "capybara"
 require "rspec"
 require "json"
 
-Capybara.register_driver :selenium do | app|
-  capabilities = {
-    :version => ENV['version'],
-    :browserName => ENV['browserName'],
-    :platform => ENV['platform'],
-  }
-
-  url = "https://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:443/wd/hub".strip
-
-  Capybara::Selenium::Driver.new(app,
-                                 :browser => :remote, :url => url,
-                                 :desired_capabilities => capabilities)
-end
-
 Capybara.default_max_wait_time = 10
 Capybara.default_driver = :selenium
 
@@ -25,17 +11,32 @@ RSpec.configure do |config|
   config.include Capybara::DSL
   config.include Capybara::RSpecMatchers
 
+  config.before(:each) do | scenario |
+    Capybara.register_driver :selenium do | app|
+      capabilities = {
+        :version => ENV['version'],
+        :browserName => ENV['browserName'],
+        :platform => ENV['platform'],
+        :name => scenario.full_description,
+        :build => ENV['build']
+      }
+
+      url = "https://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:443/wd/hub".strip
+
+      Capybara::Selenium::Driver.new(app,
+                                     :browser => :remote, :url => url,
+                                     :desired_capabilities => capabilities)
+    end
+  end
+
   config.after(:each) do | scenario |
     jobname = scenario.full_description
     sessionid = ::Capybara.current_session.driver.browser.session_id
-    ::Capybara.current_session.driver.quit
+    ::Capybara.reset_sessions!
+    ::Capybara.use_default_driver
 
     # Output sessionId and jobname to std out for Sauce OnDemand Plugin to display embeded results
     puts "SauceOnDemandSessionID=#{sessionid} job-name=#{jobname}"
-
-    job = SauceWhisk::Jobs.fetch sessionid
-    job.name = jobname
-    job.save
 
     if scenario.exception
       SauceWhisk::Jobs.fail_job sessionid
