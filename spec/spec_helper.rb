238c098560ef2d5c8881d4301f95bc600771adfa
Titus Fortner <titusfortner@gmail.com>
2019-12-04 17:06:38 -0500

    implement desired simple sauce syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'selenium-webdriver'
 require 'rspec'
-require 'sauce_whisk'
+require 'simple_sauce'
 
 Capybara.default_max_wait_time = 10
 
@@ -12,19 +12,18 @@ RSpec.configure do |config|
   config.include Capybara::DSL
   config.include Capybara::RSpecMatchers
 
+  Capybara.register_driver :sauce do |app|
+    capabilities = Sauce::Session.capybara_capabilities(platform)
+    Capybara::Selenium::Driver.new(app, **capabilities)
+  end
+
   config.before(:each) do |test|
-    Capybara.register_driver :sauce do |app|
-      opt = platform(test.full_description)
-
-      caps = Selenium::WebDriver::Remote::Capabilities.send(opt.delete(:browser_name).to_sym, opt)
-
-      url = 'https://ondemand.saucelabs.com:443/wd/hub'
-
-      Capybara::Selenium::Driver.new(app, browser: :remote,
-                                          url: url,
-                                          desired_capabilities: caps)
-    end
     Capybara.current_driver = :sauce
+
+    @session = Sauce::Session.new(generate_sauce_opts)
+    @session.name = test.full_description
+    @session.data_center = (ENV['SAUCE_DC'] || 'US_WEST').to_sym
+    @session.start
   end
 
   config.after(:each) do |test|
@@ -40,67 +39,30 @@ RSpec.configure do |config|
   # Note: not all browsers are defaulting to using w3c protocol
   # This will change soon. Where possible prefer the w3c approach
   #
-  def platform(name)
+  def platform
+    ENV['PLATFORM'] ||= 'mac_sierra_chrome'
+
     case ENV['PLATFORM']
     when 'windows_10_edge'
       {platform_name: 'Windows 10',
        browser_name: 'edge',
-       browser_version: '18.17763'}.merge(sauce_w3c(name))
+       browser_version: '18.17763'}
     when 'windows_8_ie'
       {platform: 'Windows 8.1',
        browser_name: 'ie',
-       version: '11.0'}.merge(sauce_w3c(name))
+       version: '11.0'}
     when 'mac_sierra_chrome'
-      # This is for running Chrome with w3c which is not yet the default
       {platform_name: 'macOS 10.12',
        browser_name: 'chrome',
-       "goog:chromeOptions": {w3c: true},
-       browser_version: '65.0'}.merge(sauce_w3c(name))
+       browser_version: '65.0'}
     when 'mac_mojave_safari'
       {platform_name: 'macOS 10.14',
        browser_name: 'safari',
-       browser_version: '12.0'}.merge(sauce_w3c(name))
+       browser_version: '12.0'}
     when 'windows_7_ff'
       {platform_name: 'Windows 7',
        browser_name: 'firefox',
-       browser_version: '60.0'}.merge(sauce_w3c(name))
-    else
-      # Always specify a default;
-      # this doesn't force Chrome to w3c
-      {platform: 'macOS 10.12',
-       browser_name: 'chrome',
-       version: '65.0'}.merge(sauce_oss(name))
-    end
-  end
-
-  def sauce_w3c(name)
-    {'sauce:options' => {name: name,
-                         build: build_name,
-                         username: ENV['SAUCE_USERNAME'],
-                         access_key: ENV['SAUCE_ACCESS_KEY'],
-                         iedriver_version: '3.141.59',
-                         selenium_version: '3.141.59'}}
-  end
-
-  def sauce_oss(name)
-    {name: name,
-     build: build_name,
-     username: ENV['SAUCE_USERNAME'],
-     access_key: ENV['SAUCE_ACCESS_KEY'],
-     selenium_version: '3.141.59'}
-  end
-
-  #
-  # Note that this build name is specifically for Travis CI execution
-  # Most CI tools have ENV variables that can be structured to provide useful build names
-  #
-  def build_name
-    if ENV['TRAVIS_REPO_SLUG']
-      "#{ENV['TRAVIS_REPO_SLUG'][%r{[^/]+$}]}: #{ENV['TRAVIS_JOB_NUMBER']}"
-    elsif ENV['SAUCE_START_TIME']
-      ENV['SAUCE_START_TIME']
-    else
-      "Ruby-RSpec-Capybara: Local-#{Time.now.to_i}"
+       browser_version: '60.0'}
     end
   end
 end
